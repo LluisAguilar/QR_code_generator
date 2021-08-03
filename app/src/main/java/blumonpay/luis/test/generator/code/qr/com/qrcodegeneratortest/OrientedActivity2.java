e7aafa23790c3161a02c86642b7783f672cc67cd
Luis Aguilar <laguilar@blumonpay.com.mx>
2021-08-03 10:44:15 -0500

    Initial commit

---
+++ app/src/main/java/blumonpay/luis/test/generator/code/qr/com/qrcodegeneratortest/OrientedActivity2.java
@@ -0,0 +1,112 @@
+package blumonpay.luis.test.generator.code.qr.com.qrcodegeneratortest;
+
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import java.util.Random;
+
+public class OrientedActivity2 extends CaptureActivity implements DecoratedBarcodeView.TorchListener {
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+    private ImageView switchFlashlightButton;
+    private boolean isFlashOn = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_scanner);
+
+        barcodeScannerView = findViewById(R.id.zxing_barcode_scanner);
+        barcodeScannerView.setTorchListener(this);
+
+        switchFlashlightButton = findViewById(R.id.switch_flashlight);
+
+        // if the device does not have flashlight in its camera,
+        // then remove the switch flashlight button...
+        if (!hasFlash()) {
+            switchFlashlightButton.setVisibility(View.GONE);
+        }
+
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
+
+        changeMaskColor(null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * Check if the device's camera has a Flashlight.
+     *
+     * @return true if there is Flashlight, otherwise false.
+     */
+    private boolean hasFlash() {
+        return getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+    public void switchFlashlight(View view) {
+        if (hasFlash()) {
+            if (!isFlashOn) {
+                isFlashOn = true;
+                barcodeScannerView.setTorchOn();
+            }else {
+                isFlashOn = false;
+                barcodeScannerView.setTorchOff();
+            }
+        }
+    }
+
+    public void changeMaskColor(View view) {
+        Random rnd = new Random();
+        int color = Color.argb(100, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        //viewfinderView.setMaskColor(color);
+    }
+
+    @Override
+    public void onTorchOn() {
+        Log.e("torch", "on");
+    }
+
+    @Override
+    public void onTorchOff() {
+        Log.e("torch", "off");
+    }
+}
